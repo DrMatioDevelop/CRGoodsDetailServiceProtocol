3db68e6cdf53f2c2904776c0f2bd1b90ef2b59e9
d2c_cyf <cyfsoftware@126.com>
2017-05-12 10:22:07 +0800

    Initial Commit

---
+++ CRGoodsDetailServiceProtocol/CRGoodsDetailServiceProtocol.h
@@ -0,0 +1,19 @@
+//
+//  CRGoodsDetailServiceProtocol.h
+//  CRGoodsDetailServiceProtocol
+//
+//  Created by d2c_cyf on 17/5/12.
+//  Copyright © 2017年 d2c_cyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRGoodsDetailServiceProtocol.
+FOUNDATION_EXPORT double CRGoodsDetailServiceProtocolVersionNumber;
+
+//! Project version string for CRGoodsDetailServiceProtocol.
+FOUNDATION_EXPORT const unsigned char CRGoodsDetailServiceProtocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRGoodsDetailServiceProtocol/PublicHeader.h>
+
+
